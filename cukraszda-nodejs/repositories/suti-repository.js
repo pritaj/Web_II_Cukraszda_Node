f7a25a4eef3ee92a0a0735631b875bcb7dcfae26
Rita <ritapaska@gmail.com>
2025-11-25 18:02:04 +0100

    elso

---
+++ cukraszda-nodejs/repositories/suti-repository.js
@@ -0,0 +1,37 @@
+const db = require("../module/db"); // vagy ahol a pool van
+
+async function getSutiByIdWithRelations(id) {
+  // 1) Maga a süti
+  const [sutiRows] = await db.query(
+    "SELECT id, nev, tipus, dijazott FROM suti WHERE id = ?",
+    [id]
+  );
+
+  if (sutiRows.length === 0) {
+    return null;
+  }
+
+  const suti = sutiRows[0];
+
+  // 2) Tartalomok (mentes jelzések)
+  const [tartalomRows] = await db.query(
+    "SELECT id, suti_id, mentes FROM tartalom WHERE suti_id = ?",
+    [id]
+  );
+
+  // 3) Árak
+  const [arRows] = await db.query(
+    "SELECT id, suti_id, egyseg, ertek FROM ar WHERE suti_id = ?",
+    [id]
+  );
+
+  // Laravel: $suti->tartalom, $suti->arak
+  suti.tartalom = tartalomRows;
+  suti.arak = arRows;
+
+  return suti;
+}
+
+module.exports = {
+  getSutiByIdWithRelations,
+};
